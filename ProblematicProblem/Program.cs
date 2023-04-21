f4dd5a39683317bba5c9f139f63b02c349eb9820
Levi Williams <ljw758@hotmail.com>
2023-04-20 23:35:56 -0500

    Fixed Exercise A2

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -129,12 +129,12 @@ namespace ProblematicProblem
                             //string randomNumber = rng.Next(activities.Count);
                             //string randomActivity = activities[randomNumber];
                         }
-                        Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+                        Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
                         Console.WriteLine();
                         var userLastResponse = Console.ReadLine().ToLower();
                         if (userLastResponse == "redo") { cont = true; }
-                        else { cont = false; Console.WriteLine($"Enjoy your {randomActivity}, goodbye now!"); };
-
+                        if (userLastResponse == "keep") { cont = false; Console.WriteLine($"Enjoy your {randomActivity}, goodbye now!");; };
+                        
                     }
                 }
             
